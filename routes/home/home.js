42e336940abb82ca801881cb484d2675e5050bbc
chenrui <chenrui@mini1.cn>
2019-09-12 22:32:56 +0800

    增加标签查询sql

--- routes/home/home.js
+++ routes/home/home.js
@@ -2,6 +2,7 @@ const router = require('koa-router')()
 const sql = require('../../sql/home.sql')
 
 router
+    //首页数据
     .get('/home', async (ctx, next) => {
         let {
             pageSize
@@ -17,13 +18,26 @@ router
         ctx.body = result
 
     })
+    //帖子详情
     .get('/post', async(ctx, next) => {
         let { post_id } = ctx.query
         const result = await sql.SELECT_CONTANT(post_id)
         ctx.body = result[0]
     })
-    .get('/homePv', async(ctx, next) => {
-        
+    //标签页
+    .get('/tags', async(ctx, next) => {
+        const res = await sql.FIND_TAGS()
+        let _r = res.map( res => {
+            let r = res.tags.split(",")
+            res.tags = r
+            return res
+        })
+        let result = []
+        _r.map( res => {
+            result = result.concat(res.tags)
+        })
+        console.log(res)
+        ctx.body = [...new Set(result)]
     })
 
 module.exports = router
--- sql/home.sql.js
+++ sql/home.sql.js
@@ -4,7 +4,7 @@ const ADMIN = {
     SELECT_LIST: (...arg) => query.query(`SELECT tags, title, id, time FROM Blog_contant limit ${8*(arg[0] - 1)}, 8`),     // 获取contant表数据 {name} 列名
     SELECT_CONTANT : (id) => query.query(`SELECT contant FROM Blog_contant where id="${id}"`),  //查询特定的帖子
     SELECT_COUNT : () =>query.query(`select count(*) from Blog_contant`),  //查询帖子总数
-    FIND_ADMIN : () =>query.query(`select count(*) num from Admin`),  
+    FIND_TAGS : () =>query.query(`select tags, title from Blog_contant`),  
     FIND_ADMIN_AGE : () =>query.query(`select age,COUNT(id) id from Admin group by age having count(age) > 0`),
     ADD_AGE: () => query.query(
         `SELECT a.dat,a.num,SUM(lt.num)  AS cum FROM
